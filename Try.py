f617e50bbe315df09e1e979d467f1476e275bb2d
Kingleo007 <kingleo000077@gmail.com>
2025-09-22 09:17:27 +0530

    Works?

--- Try.py
+++ Try.py
@@ -1,45 +1,5 @@
-"""def average(a,b):
-    avg = ((a+b)/2)
-    return 0 #after this function ends
-    return avg #this line is ignored
-print(average(5,5))
-"""
-
-"""def fun(a,b):
-    print(a)
-    print(b)
-    print(a+b)
-fun(5,a=7) """
-
-"""def testing(a,b,c):
-    c=2
-    print(a+b+c)
-
-testing(1,2)"""
-"""x = 2  # global 
-def test():
-    x =1 #local 
-    return x
-print(x)
-print(test())"""
-
-"""def add(a,b):
-    return a+b
-r1 = add(5,3)
-r2 = add(2,3)
-print(r1+r2)"""
-
-"""
-def max_of_three(a,b,c):
-    d = [a,b,c]
-    e = max(d)
-    return e
-a,b,c = map(int,input().split())
-print(max_of_three(a,b,c))"""
-
-def add(a,b):
-    return a+b
-
-def addition(a,b,c):
-    return a+b+c
-print(add(1,2))
+def a():
+    print("yo")
+    return 0
+a()
+print(a())
